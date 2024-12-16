638ce17fc6623c82f7edb0c6aeb13d9227762eba
UH3135 <rabbitdy0101@kookmin.ac.kr>
2024-12-16 13:53:14 +0900

    FEAT: SQLALCHEMY를 이용해서 데이터베이스 추가

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,11 +1,26 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 
 load_dotenv()
 
+from config import DevelopmentConfig
+
+# 다른 모듈에서 사용 가능하도록 구성
+db = SQLAlchemy()
+migrate = Migrate()
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(DevelopmentConfig)
 
+    #ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
+
+    #blueprint
     from .views import main_view
     app.register_blueprint(main_view.bp)
 
